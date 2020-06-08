684a55abbff3c37d49f56ccbd71e5b4475f97dc3
roulamokhtar <roula_mokhtar2001@yahoo.fr>
2020-06-08 21:32:12 +0100

    add whitenoise.runserver_nostatic

--- sisig/settings.py
+++ sisig/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = ['incendies.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'incendies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'leaflet',
