9f303109a116443b6b347e801aa02b4265b44c79
roulamokhtar <roula_mokhtar2001@yahoo.fr>
2020-06-08 12:20:17 +0100

    retour de staticfile in setting

--- sisig/settings.py
+++ sisig/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.templatetags.static',
+    'django.contrib.staticfiles',
     'django.contrib.gis',
     'leaflet',
     'import_export',
