431dee5341344ab5e98b2b37e2d7f301cb5bf1a1
roulamokhtar <roula_mokhtar2001@yahoo.fr>
2020-06-11 00:54:52 +0100

    actualise

--- sisig/settings.py
+++ sisig/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ox-5!jmktn=n@^#dhckk9$^pvxic#q*gk-z#h=ygh5k@vwz=dc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
