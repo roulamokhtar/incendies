2464a6d1c90ec6f21649bc46e5377d2c47ae33f2
roulamokhtar <roula_mokhtar2001@yahoo.fr>
2020-06-13 21:14:25 +0100

    nouveau disign

--- incendies/admin.py
+++ incendies/admin.py
@@ -1,5 +1,7 @@
 from django.contrib.gis import admin
 from .models import *
+from leaflet.admin import LeafletGeoAdmin
+
 from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
